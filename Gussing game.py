e28fb18317fddbffe1557663256566264f8cfb3a
MaxBlockley <78321099+MaxBlockley@users.noreply.github.com>
2021-02-16 14:24:38 +1300

    gusseing the game

--- Gussing game.py
+++ Gussing game.py
@@ -1,19 +1,46 @@
 import random
-#set boolean to true
-keep_asking=True
+high_score = 1000000
 
-#Ask them for their number
-G_NUMBER = random.randint(1,10)
-while keep_asking==True:
-    number=int(input("Guess a number between 1 and 10"))
+keep_playing = True
+while keep_playing == True:
+    #set boolean to true
+    keep_asking=True
+    counter=0
 
-    if number == G_NUMBER :
-        keep_asking=False
+    #Ask them for their number
+    G_NUMBER = random.randint(1,100)
+
+    while keep_asking==True:
+        #add one to the counter
+        counter += 1
+        number=int(input("Guess a number between 1 and 100"))
+
+        if number == G_NUMBER :
+            keep_asking=False
+        else:
+            print("wrong number")
+            if number > G_NUMBER:
+                print("To high")
+            elif number < G_NUMBER:
+                print("to Low")
+
+
+    if counter <= high_score :
+        print("contgrat you got high score")
+        high_score = counter
+
+    print("Congratulations you guessed correct")
+    print("you took {} guess".format(counter))
+    y_n = input("play again y/n?")
+
+    if y_n == "y":
+        keep_playing = True
+
     else:
-        print("wrong number")
-        if number > G_NUMBER:
-            print("To high")
-        elif number < G_NUMBER:
-            print("to Low")
+        keep_playing = False
 
-print("That is correct")
+
+
+
+
+
